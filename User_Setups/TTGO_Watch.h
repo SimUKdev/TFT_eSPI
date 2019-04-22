d68a6c3c16c26df8d996043f2b40cca0477fa84f
lewisxhe <lewishe@outlook.com>
2019-04-22 11:22:36 +0800

    add T-Watch LCD def

---
+++ User_Setups/TTGO_Watch.h
@@ -0,0 +1,20 @@
+
+
+#define TFT_WIDTH  240
+#define TFT_HEIGHT 240
+#define ST7789_DRIVER
+
+#define TFT_MISO            4
+#define TFT_MOSI            19
+#define TFT_SCLK            18
+#define TFT_CS              5
+#define TFT_DC              27
+
+// #define SPI_FREQUENCY   1000000
+// #define SPI_FREQUENCY   5000000
+// #define SPI_FREQUENCY  10000000
+// #define SPI_FREQUENCY  20000000
+// #define SPI_FREQUENCY 27000000 // Actually sets it to 26.67MHz = 80/3
+// #define SPI_FREQUENCY  40000000 // Maximum to use SPIFFS
+#define SPI_FREQUENCY  80000000
+
