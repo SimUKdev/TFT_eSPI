d594f6729510600e97d5b10cf9d8271d1fd3f46b
lewisxhe <lewishe@outlook.com>
2019-06-10 17:35:58 +0800

    add 1.14 inc st7789v support

--- TFT_Drivers/ST7789_Rotation.h
+++ TFT_Drivers/ST7789_Rotation.h
@@ -7,28 +7,48 @@
      writedata(TFT_MAD_RGB);
       _width  = _init_width;
       _height = _init_height;
+  #ifdef TTGO_114_INC_DISPLAY
+      colstart = 52;
+      rowstart = 40;
+  #else
        colstart = 0;
        rowstart = 0;
+  #endif
      break;
    case 1: // Landscape (Portrait + 90)
      writedata(TFT_MAD_MX | TFT_MAD_MV | TFT_MAD_RGB);
       _width  = _init_height;
       _height = _init_width;
+  #ifdef TTGO_114_INC_DISPLAY
+      colstart = 40;
+      rowstart = 53;
+  #else
        colstart = 0;
        rowstart = 0;
+  #endif
      break;
    case 2: // Inverter portrait
      writedata(TFT_MAD_MX | TFT_MAD_MY | TFT_MAD_RGB);
       _width  = _init_width;
       _height = _init_height;
+  #ifdef TTGO_114_INC_DISPLAY
+      colstart = 53;
+      rowstart = 40;
+  #else
        colstart = 0;
        rowstart = 80;
+  #endif
      break;
    case 3: // Inverted landscape
      writedata(TFT_MAD_MV | TFT_MAD_MY | TFT_MAD_RGB);
       _width  = _init_height;
       _height = _init_width;
+  #ifdef TTGO_114_INC_DISPLAY
+      colstart = 40;
+      rowstart = 52;
+  #else
        colstart = 80;
        rowstart = 0;
+  #endif
      break;
   }
